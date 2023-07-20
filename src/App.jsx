97675815c61029379013fce0b649ec78d3650dc5
Sanskar-12 <sanskarv2004@gmail.com>
2023-07-20 22:27:30 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./assets/app.scss"
+
+function App() {
+
+  return (
+    
+        <div className="product">
+          <p className="p1">Click 2</p>
+          <p className="p2">Click 2</p>
+          <p className="p3">Click 2</p>
+        </div>
+    
+  )
+}
+
+export default App
